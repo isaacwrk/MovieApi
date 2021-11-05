b6ddb54042d9472474215224e06e71634156b7a2
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-05 09:54:51 -0300

    finishing initial design in header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="font-Oswald text-center mt-1">Moviets</div>
+    <Header/>
     <MainPage/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import Header from '@/components/Header.vue';
 import MainPage from '@/views/MainPage.vue';
+
 
 
 @Options({
     components: {
-        MainPage
+        MainPage,
+        Header
     },
 })
 export default class App extends Vue {}
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="flex items-center justify-between flex-wrap bg-gray-600 p-6 font-Oswald">
+        <div class="flex items-center flex-no-shrink text-white mr-6">
+            <img src="~@/assets/logo.png" style="height:50px">
+            <span class="font-light text-2xl tracking-tight">MovieSeacher API</span>
+        </div>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div class="text-sm lg:flex-grow">
+                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-200 mr-4">
+                    Api Documentation
+                </a>
+                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-200 mr-4">
+                    Repository
+                </a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const Header = defineComponent({
+});
+
+export default Header;
+</script>
+
+<style>
+
+</style>
